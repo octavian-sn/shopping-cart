ac90635fb40fe94ee9aac9784f5f8843f3c66927
Sulic Octavian <octavius1992@gmail.com>
2023-03-06 17:43:22 +0200

    Add content component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { Link, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Content from './components/Content';
 import './styles/app.css';
 
 function App() {
   return (
     <div id="app">
       <Header />
+      <Content />
     </div>
   );
 }
---
+++ src/components/Content.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/content.css';
+
+function Content() {
+  return (
+    <div id="content">
+      <div className="overlay"></div>
+    </div>
+  );
+}
+
+export default Content;
