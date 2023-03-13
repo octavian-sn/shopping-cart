06978c8248402176b95104a5f1246603f13c5648
Sulic Octavian <octavius1992@gmail.com>
2023-03-13 09:39:55 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ContextProvider>
-      <BrowserRouter basename="/">
+      <BrowserRouter basename={'/'}>
         <App />
       </BrowserRouter>
     </ContextProvider>
