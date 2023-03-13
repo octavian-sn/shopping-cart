fd99e051bf03ca8f95ab32284a46676b15127dfa
Sulic Octavian <octavius1992@gmail.com>
2023-03-13 09:23:26 +0200

    Update package.json

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ContextProvider>
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <App />
       </BrowserRouter>
     </ContextProvider>
